4dedfd17fd7739819257c2ba570f0ef540fd0b22
Maanfee <Maanfee@outlook.com>
2026-02-10 10:00:07 +0330

    6

--- DashboardAPI/Maanfee.Dashboard.Views.Core/Layout/_BaseLayout.cs
+++ DashboardAPI/Maanfee.Dashboard.Views.Core/Layout/_BaseLayout.cs
@@ -41,5 +41,23 @@ namespace Maanfee.Dashboard.Views.Core
             Snackbar?.Configuration.PositionClass = (SharedLayoutSettings.IsRTL) ? Defaults.Classes.Position.BottomStart : Defaults.Classes.Position.BottomEnd;
         }
 
+        protected virtual async Task OpenInformationDialog()
+        {
+            DialogParameters DialogParameters = new DialogParameters();
+            DialogParameters.Add("ComponentName", this.GetType().Name);
+            DialogParameters.Add("ComponentFullName", this.GetType().FullName!);
+
+            await Dialog!.ShowAsync<InformationDialog>(string.Empty, DialogParameters,
+                 new DialogOptions()
+                 {
+                     NoHeader = true,
+                     MaxWidth = MaxWidth.Small,
+                     FullWidth = true,
+                     Position = DialogPosition.Center,
+                     BackgroundClass = "Dialog-Blur",
+                     CloseOnEscapeKey = true,
+                 });
+        }
+
     }
 }
--- DashboardAPI/Maanfee.Dashboard.Views.Core/Views/_BaseView.cs
+++ DashboardAPI/Maanfee.Dashboard.Views.Core/Views/_BaseView.cs
@@ -1,26 +1,7 @@
-﻿using MudBlazor;
-
-namespace Maanfee.Dashboard.Views.Core
+﻿namespace Maanfee.Dashboard.Views.Core
 {
     public class _BaseView : _BaseLayout
     {
-        protected virtual async Task OpenInformationDialog()
-        {
-            DialogParameters DialogParameters = new DialogParameters();
-            DialogParameters.Add("ComponentName", this.GetType().Name);
-            DialogParameters.Add("ComponentFullName", this.GetType().FullName!);
-
-            await Dialog!.ShowAsync<InformationDialog>(string.Empty, DialogParameters,
-                 new DialogOptions()
-                 {
-                     NoHeader = true,
-                     MaxWidth = MaxWidth.Small,
-                     FullWidth = true,
-                     Position = DialogPosition.Center,
-                     BackgroundClass = "Dialog-Blur",
-                     CloseOnEscapeKey = true,
-                 });
-        }
 
     }
 }
